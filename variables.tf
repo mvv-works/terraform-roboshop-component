ac105c755c1c33abaf1a6ae971ae397cff2abedd
vamsi <vijayvamsi550@gmail.com>
2026-07-24 15:13:53 +0530

    roboshop-dev-infra

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "app_version" {
+  default = "v3"
+}
+
+variable "component" {
+  type = string
+}
+
+variable "domain_name" {
+  default = "devops550.online"
+}
+
+
